d2801b3387d88b630b6d31262e1fa8adf11a493f
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-11-01 15:07:34 -0700

    Updated robot code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/hardware/LinearSlide.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.huskyteers.hardware;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class LinearSlide {
+    private DcMotor motor;
+    public LinearSlide(HardwareMap hardwareMap) {
+        motor = hardwareMap.get(DcMotor.class, "slide");
+    }
+
+    public class MoveSlide implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                motor.setPower(0.8);
+                initialized = true;
+            }
+
+            motor.setTargetPosition(2);
+
+//            double vel = motor.velo
+//                    ;
+//            packet.put("shooterVelocity", vel);
+//            return vel < 10_000.0;
+            return false;
+        }
+    }
+    public Action raiseUp() {
+        return new MoveSlide();
+    }
+}
