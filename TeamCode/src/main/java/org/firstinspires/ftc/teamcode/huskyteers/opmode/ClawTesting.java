2100bb9059b504f377fd27c30e3f92023a7b021c
Ethan <ehtanhnery@gmail.com>
2024-11-26 22:52:19 -0800

    Update ClawTesting.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/opmode/ClawTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/huskyteers/opmode/ClawTesting.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @TeleOp
-public class HuskyTeleOp extends HuskyOpMode {
+public class ClawTestingOpMode extends HuskyOpMode {
     @Override
     public void runOpMode() {
         initVisionPortal(StartInfo.Color.BLUE);
